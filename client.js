eef9494e6ff9d1ed5a6e5cd9c74b9b0e3e1335ab
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-12-26 13:36:02 +0700

    NodeJS-Async
    NodeJS-Async

---
+++ client.js
@@ -0,0 +1,14 @@
+var net = require('net')
+var HOST = '127.0.0.1'
+var PORT = 6969
+var client = new net.Socket()
+client.connect(PORT, HOST, function() {
+        client.write('9')
+})
+client.on('data',function(data) {
+    console.log('DATA: ' + data)
+    client.destroy()
+})
+client.on('close',function() {
+    console.log('Connected closed')
+})
